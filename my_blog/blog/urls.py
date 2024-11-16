6313b79e0299b796f38f3df8eb41ec164f25bf15
SilasHakuzwimana <hakusilas@gmail.com>
2024-11-16 20:35:41 +0200

    add modfication

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path # type: ignore
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('posts/', views.post_list, name='post_list'),
 ]
