cda0e80a2a7d376fc5a71153592807cebc9a6002
SilasHakuzwimana <hakusilas@gmail.com>
2024-11-16 21:54:59 +0200

    update posts

---
+++ my_blog/blog/forms.py
@@ -0,0 +1,7 @@
+from django import forms # type: ignore
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'author']
