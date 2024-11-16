e0028ea22b3446c4829da6afc6e2d5d6530ed5c3
SilasHakuzwimana <hakusilas@gmail.com>
2024-11-16 22:26:08 +0200

    add comment fucntionlity

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404 # type: ignore
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.objects.all()
@@ -8,7 +9,16 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
 
 def post_new(request):
     if request.method == "POST":
